b8d69ed36469830900701b6617ba122ac7e16c6f
corina <ciorpaccorina@gmail.com>
2023-02-01 08:55:17 +0100

    realizacion ej2

---
+++ js/main.js
@@ -0,0 +1,69 @@
+/*1. Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor
+de los dos. Si son iguales indicarlo también. Ves cambiando los valores para
+comprobar que funciona.*/
+ 
+ 
+ 
+ 
+ 
+ /*console.log("hola mundo")
+ let num1 = parseInt(prompt("Introduce el primer numero"))
+ let num2 = parseInt(prompt("Introduce el segundo numero numero"))
+if(num1 < num2){
+     console.log(num2, "es mayor que" ,num1);
+ }else if(num1 === num2){
+    console.log("Son iguales ")
+ }else {
+     console.log( num1," es mayor que ", num2);
+ }*/
+
+ /*2. Declara un String que contenga tu nombre, después muestra un mensaje de
+bienvenida por consola. Por ejemplo: si introduzco “Fernando”, me aparezca
+“Bienvenido Fernando”.*/
+ /*var nombre = ("corina") //prueba1//
+ var nombre = ("elena")//prueba2//
+ console.log(" Bienvenido " +  nombre)*/
+
+
+ /*3. Modifica la aplicación anterior, para que nos pida el nombre que queremos
+introducir (recuerda usar prompt).*/
+
+/*let = ("nombre")
+nombre = prompt("Dime tu nombre");
+console.log(" bienvenido " + nombre)*/
+
+/*Haz una aplicación que calcule el área de un círculo (pi*R2). El radio se pedirá
+por teclado (recuerda pasar de String a double con parseDouble). Usa la
+constante PI.*/
+/*Lee un número por teclado e indica si es divisible entre 2 (resto = 0). Si no lo es,
+también debemos indicarlo.*/
+
+ /*let num = parseInt (prompt("Introduce el  numero"))
+
+if (num % 2 == 0 ){
+console.log("es divisible por 2")
+}else{
+console.log("no es divisible por 2")
+}*/
+// && AND
+
+// || OR
+
+// != NO
+/*/6.Lee un número por teclado que pida el precio de un producto (puede tener
+decimales) y calcule el precio final con IVA. El IVA sera una constante que sera
+del 21%*/
+
+const IVA = 0.21
+let = precio
+precio = parseInt (prompt("Introduce el  precio"))
+ 
+ precioFinal = precio + ( precio*IVA)
+ console.log(precioFinal)
+
+/*7. Muestra los números del 1 al 100 (ambos incluidos). Usa un bucle while.*/
+/*let num = 1
+while (num <= 100){
+console.log(num );num ++
+}*/
+ 
